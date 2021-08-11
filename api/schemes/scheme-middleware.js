7b0fa770d8ff747b69ceafb38de88d0f5d3105dc
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-11 14:33:16 -0500

    middleware wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,42 +1,52 @@
-/*
-  If `scheme_id` does not exist in the database:
+const Scheme = require('../schemes/scheme-model')
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = (req, res, next) => {
-
+  try {
+    const { id } = req.params
+    const scheme = Scheme.findById(id)
+      if(!scheme){
+        res.status(404).json(`Scheme with id:${id} not found`)
+      } 
+      else {
+        req.scheme = scheme
+        next()
+      }
+  }
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if( scheme_name === undefined ||
+      typeof scheme_name !== 'string' ||
+      !scheme_name.trim()
+  ) {
+    next({status:400, message:'Scheme id not found'})
+  }
+  else {
+    next()
+  }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+  if(!instructions) {
+    res.status(400).json('Instructions field is required')
+  }
+  else if(step_number < 1) {
+    res.status(400).json('Step not found')
+  }
+  else {
+    next()
+  }
 }
 
 module.exports = {
   checkSchemeId,
   validateScheme,
-  validateStep,
+  validateStep
 }
